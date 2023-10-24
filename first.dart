83d914f62f26423a2d18154ee0cc029ada12341b
Lenkaa3792 <allanlenkaa@gmail.com>
2023-10-24 21:22:53 +0300

    done first dart code

---
+++ first.dart
@@ -0,0 +1,10 @@
+void main() {
+  //my basic dart code(single line comment)
+  print("Hello world");
+/*
+we are kilifi team
+we are GOA TEAM
+ this is PLP 
+*/ 
+  print("This is kilifi and GOA cohort");
+}
---
+++ second.dart
@@ -0,0 +1,13 @@
+// we are talking about variables in dart
+/*
+variables are containers that store values
+variables can be denoted using the word var 
+
+*/
+void main() {
+  var name = "Ronald Ngala";
+  print(name);
+  //variables can also be denoted using data types
+  String school = "POWER LEARN ACADEMY";
+  print(school);
+}
